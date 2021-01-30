cbeed7e84bd0f9dc3332ad7ac91e12d5f1ea8976
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-01-30 12:21:32 -0600

    added Expel button to card

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ const createStudentCards = (arr) => {
       <h5 class="card-title">${item.name}</h5>
       <h6 class="card-subtitle mb-2 text-muted">${item.house}</h6>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <button type="button" class="btn btn-primary">Expel</button>
     </div>
   </div>`
   }
