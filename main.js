550664e23065b25cca4d69bf38d1ff6c15954247
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-01-30 12:16:32 -0600

    Add functionality to print student cards to DOM

--- main.js
+++ main.js
@@ -2,7 +2,35 @@ console.log('CONNECTED');
 
 const students = [
   {
-    name, 
-    
+    name: 'Gabby', 
+    house: 'Slytherin',
+
   }
 ];
+
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+
+const createStudentCards = (arr) => {
+  let domString = '';
+  for (let item of arr) {
+    const i = arr.indexOf(item);
+    domString += `<div class="card" style="width: 18rem;" id=${i}>
+    <div class="card-body">
+      <h5 class="card-title">${item.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${item.house}</h6>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>`
+  }
+  printToDom('#students', domString);
+}
+
+const init = () => {
+  createStudentCards(students);
+}
+
+init();
