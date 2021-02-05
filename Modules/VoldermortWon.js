d100c4151d6a027748e99d5b2c5351675eded55d
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-02-05 13:00:56 -0600

    added a module for one of the callback functions

---
+++ Modules/VoldermortWon.js
@@ -0,0 +1,6 @@
+const VoldemortWon = () => {
+  document.querySelector('.jumbotron').style.display = "none";
+  document.querySelector('#createForm').style.display = "none";
+};
+
+export default VoldemortWon;
